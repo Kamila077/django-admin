05ccfa384912ba6f503a9734f5c57b2a34637c0d
Kamila077 <114273837+Kamila077@users.noreply.github.com>
2023-02-27 12:05:56 +0600

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from .views import BlogList, BlogDetail
-
-urlpatterns = [
-    path('api/blogs/', BlogList.as_view(), name='blog-list'),
-    path('api/blogs/<int:pk>/', BlogDetail.as_view(), name='blog-detail'),
-    path('', include('blogs.urls')),
-]
